8a76a1fd84a1600a2e207121a411b3cc4bbd2fe6
nandito <ruby@nandito.info>
2011-11-05 06:16:21 -0700

    *Important first import

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+RoRhf::Application.config.secret_token = '57e864a23b8a1078365906310aff572c7a59987c14c1c54377e47e8505e918e8592608d44a37e19c09ef294fd8c0fd830e0b10968f0649521970b8b00d2b455e'
