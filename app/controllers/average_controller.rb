12fbfc8c13848fcb9ad338e521b4b39548d137fe
nandito <ruby@nandito.info>
2011-11-17 12:51:49 -0800

    * modositva 111117 2151

--- app/controllers/average_controller.rb
+++ app/controllers/average_controller.rb
@@ -1,12 +1,19 @@
 class AverageController < ApplicationController
   def index
 	@subjects = Subject.all
-	@result =''
+	@result = ''
 	@kreditek = ''
+#	@jegyek = ''
   end
   def szamol
 	@subjects = Subject.all
-	@result = params[:f].to_i + params[:s].to_i
+#	@result = params[:f].to_i + params[:s].to_i
+#	jegyek = Array.new
+	@subjects.each do |i|
+#		@jegyek = Array.new(:i.to_i)
+		@result = i
+	end
+#	@result = @jegyek.sum
 	@kreditek = Subject.sum(:credit)
 	
 	render :action => :index
